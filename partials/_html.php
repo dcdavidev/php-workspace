94d9ab1ca17067ce479d8222ae963b08c046faf1
Davide Di Criscito <davidedc@outlook.com>
2025-10-29 19:56:16 +0100

    chore: create workspace

---
+++ partials/_html.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?php echo $LANG ?? "it"; ?>">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+  <title><?php echo $PAGE_TITLE; ?></title>
+  <link rel="stylesheet" href="/assets/styles/normalize.css">
+  <link rel="stylesheet" href="/assets/styles/main.css">
+</head>
+<body>
+  
+</body>
+</html>
